f4421f24a6220e0fb6c4d288f718f8f470aabdf0
shivang2056 <64776242+shivang2056@users.noreply.github.com>
2024-04-19 16:49:16 +0530

    Task: Add ability to search product on Discover page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = Product.with_name(params[:query])
   end
 
   # GET /products/1 or /products/1.json
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  scope :with_name, ->(name) { where("name ilike '%#{name}%'") if name.present? }
 end
