c5d9b6a9e29684cb869f85df360c39f30c3a9741
shivang2056 <64776242+shivang2056@users.noreply.github.com>
2024-05-29 20:26:32 +0530

    Enhancement: Modify design for discover page.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :user
 
-  has_one_attached :image
   has_one :financial, class_name: "ProductFinancial"
 
   scope :with_name, ->(name) { where("name ilike '%#{name}%'") if name.present? }
   scope :active, -> { where(active: true) }
 
   validates :name, :description, :price, presence: true
+
+  has_one_attached :image do |image|
+    image.variant :card, resize_to_limit: [350, 240]
+  end
 end
