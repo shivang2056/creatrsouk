d748c7edf6bb1868211cfd7b3787e82040c33757
shivang2056 <64776242+shivang2056@users.noreply.github.com>
2024-05-31 01:00:30 +0530

    Enhancement: Modify design for product edit & new pages.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,17 @@
 module ProductsHelper
+
+  def back_button_config
+    if product_created_by_current_user?
+      label = "Back to My Products"
+      path = my_products_path
+    else
+      label = "Back to Discover"
+      path = discover_path
+    end
+
+    {
+      label: label,
+      path: path
+    }
+  end
 end
