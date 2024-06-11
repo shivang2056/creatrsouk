4b3a71d04e9a5f11710c24126411882fc1238dc9
shivang2056 <64776242+shivang2056@users.noreply.github.com>
2024-06-11 15:10:56 +0530

    Enhancement: Add message to product#show and user_purchase#show if attachments not present.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,6 @@
 module ProductsHelper
 
-  def back_button_path
+  def product_show_back_button_path
     request.referrer || ( product_created_by_current_user? ? my_products_products_path : discover_products_path )
   end
 end
